343455f2e4699898b5e5687e8214c10feea2ad3f
Frédéric DUPERIER <fduperier@gmail.com>
2016-08-14 12:22:16 +0200

    Migrating to Angular2 RC5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 
-import { SummaryPipe } from './posts/shared';
-import { FromNowPipe } from './shared';
-
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css'],
-  pipes: [ SummaryPipe, FromNowPipe ]
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
